17c0b57315ef7316ec29ddede6bc189670a4af95
Pryambudhi Cahyadi <pcahyadi@gmail.com>
2019-04-19 21:10:08 -0400

    i fixed the card body

--- client/src/components/BookList/index.js
+++ client/src/components/BookList/index.js
@@ -7,25 +7,25 @@ import Button from "../Button";
 class BookList extends React.Component {
     render() {
         return (
-            <Col size="md-12">
+            <Col sm={12} className="card-wrapper">
                 <Card>
                     <Card.Body>
                         <Row>
-                            <Col size="xs-8 sm-9">
+                            <Col size="xs-8">
                                 <h3>{this.props.title}</h3>
                                 <p>Author:{this.props.authors ? this.props.authors.join(', ') : 'N/A'}</p>
                             </Col>
-                            <Col size="xs-4 sm-2">
+                            <Col size="xs-4">
                                 <a href={this.props.link ? this.props.link : '#'} role="button" className="badge badge-info">
                                 View
                                 </a>
                             </Col>
                         </Row>
                         <Row>
-                            <Col size="xs-4 sm-2">
+                            <Col size="xs-4">
                                 <img src={this.props.image ? this.props.image : ""} alt={this.props.title} />
                             </Col>
-                            <Col size="xs-8 sm-9">
+                            <Col size="xs-8">
                                 {this.props.description ? this.props.description : 'N/A'}
                                 <br />
                                 <Button role="button" className="btn btn-danger btn-sm" onClick={() => this.props.onSelect()}>
