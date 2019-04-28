05a1ef1fdc142505c5a07807180dbad3f3def1b7
Pryambudhi Cahyadi <pcahyadi@gmail.com>
2019-04-28 07:07:57 -0400

    i fixed the saving issue

--- models/book.js
+++ models/book.js
@@ -1,13 +1,14 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  synopsis: String,
-  date: { type: Date, default: Date.now }
+    title: { type: String, required: true },
+    authors: [{ type: String}],
+    description: String,
+    image: String,
+    link: { type: String, required: true }
 });
 
-const Book = mongoose.model("Book", bookSchema);
+const Book = mongoose.model('Book', bookSchema);
 
 module.exports = Book;
